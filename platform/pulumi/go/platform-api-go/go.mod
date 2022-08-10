026a19c5d74f3a5de19ea03059d02fd47e0707e6
salaboy <Salaboy@gmail.com>
2022-08-10 08:25:10 +0100

    initial pulumi app working

--- platform/pulumi/go/platform-api-go/go.mod
+++ platform/pulumi/go/platform-api-go/go.mod
@@ -5,27 +5,46 @@ go 1.17
 require (
 	github.com/pulumi/pulumi-kubernetes/sdk/v3 v3.19.4
 	github.com/pulumi/pulumi/sdk/v3 v3.35.3
+	k8s.io/client-go v0.24.3
 )
 
 require (
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
 	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da // indirect
 	github.com/blang/semver v3.5.1+incompatible // indirect
 	github.com/cheggaaa/pb v1.0.18 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/djherbis/times v1.2.0 // indirect
+	github.com/emicklei/go-restful v2.9.5+incompatible // indirect
 	github.com/emirpasic/gods v1.12.0 // indirect
+	github.com/go-logr/logr v1.2.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.5 // indirect
+	github.com/go-openapi/swag v0.19.14 // indirect
 	github.com/gofrs/uuid v3.3.0+incompatible // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/glog v0.0.0-20160126235308-23def4e6c14b // indirect
-	github.com/golang/protobuf v1.4.2 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/gnostic v0.5.7-v3refs // indirect
+	github.com/google/gofuzz v1.1.0 // indirect
 	github.com/grpc-ecosystem/grpc-opentracing v0.0.0-20180507213350-8e809c8a8645 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-multierror v1.0.0 // indirect
+	github.com/imdario/mergo v0.3.5 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/kevinburke/ssh_config v0.0.0-20190725054713-01f96b0aa0cd // indirect
+	github.com/mailru/easyjson v0.7.6 // indirect
 	github.com/mattn/go-runewidth v0.0.8 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/mitchellh/go-ps v1.0.0 // indirect
+	github.com/moby/spdystream v0.2.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/opentracing/basictracer-go v1.0.0 // indirect
 	github.com/opentracing/opentracing-go v1.1.0 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
@@ -43,17 +62,31 @@ require (
 	github.com/uber/jaeger-lib v2.2.0+incompatible // indirect
 	github.com/xanzy/ssh-agent v0.2.1 // indirect
 	go.uber.org/atomic v1.6.0 // indirect
-	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
-	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
-	golang.org/x/sys v0.0.0-20210817190340-bfb29a6856f2 // indirect
-	golang.org/x/text v0.3.3 // indirect
-	google.golang.org/genproto v0.0.0-20200608115520-7c474a2e3482 // indirect
-	google.golang.org/grpc v1.29.1 // indirect
-	google.golang.org/protobuf v1.24.0 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8 // indirect
+	golang.org/x/sys v0.0.0-20220209214540-3681064d5158 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20220210224613-90d013bbcef8 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/genproto v0.0.0-20210402141018-6c239bbf2bb1 // indirect
+	google.golang.org/grpc v1.36.1 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/inf.v0 v0.9.1 // indirect
 	gopkg.in/src-d/go-billy.v4 v4.3.2 // indirect
 	gopkg.in/src-d/go-git.v4 v4.13.1 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	k8s.io/api v0.24.3 // indirect
+	k8s.io/apimachinery v0.24.3 // indirect
+	k8s.io/klog/v2 v2.60.1 // indirect
+	k8s.io/kube-openapi v0.0.0-20220328201542-3ee0da9b0b42 // indirect
+	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
 	lukechampine.com/frand v1.4.2 // indirect
+	sigs.k8s.io/json v0.0.0-20211208200746-9f7c6b3444d2 // indirect
+	sigs.k8s.io/structured-merge-diff/v4 v4.2.1 // indirect
+	sigs.k8s.io/yaml v1.2.0 // indirect
 	sourcegraph.com/sourcegraph/appdash v0.0.0-20190731080439-ebfcffb1b5c0 // indirect
 )
--- platform/pulumi/go/platform-api-go/main.go
+++ platform/pulumi/go/platform-api-go/main.go
@@ -1,44 +1,208 @@
 package main
 
 import (
-	appsv1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/apps/v1"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes"
 	corev1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/core/v1"
-	metav1 "github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/meta/v1"
+	"github.com/pulumi/pulumi-kubernetes/sdk/v3/go/kubernetes/helm/v3"
 	"github.com/pulumi/pulumi/sdk/v3/go/pulumi"
+	clientKubernetes "k8s.io/client-go/kubernetes"
+	clientCmd "k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/tools/portforward"
+	"k8s.io/client-go/transport/spdy"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
 )
 
 func main() {
+
+	factory := EnvironmentFactory{
+		Environment: Environment{
+			Name:      "dev-environment",
+			Kind:      "dev",
+			Size:      "medium",
+			GitOpsURL: "",
+		},
+	}
+
+	factory.createEnvironment()
+
+}
+
+type Environment struct {
+	Name      string `json:"name"`
+	Kind      string `json:"kind"`
+	Size      string `json:"size"`
+	GitOpsURL string `json:"git_ops_url"`
+}
+
+type EnvironmentFactory struct {
+	Environment Environment
+}
+
+func (envFactory *EnvironmentFactory) createEnvironment() error {
 	pulumi.Run(func(ctx *pulumi.Context) error {
 
-		appLabels := pulumi.StringMap{
-			"app": pulumi.String("nginx"),
-		}
-		deployment, err := appsv1.NewDeployment(ctx, "app-dep", &appsv1.DeploymentArgs{
-			Spec: appsv1.DeploymentSpecArgs{
-				Selector: &metav1.LabelSelectorArgs{
-					MatchLabels: appLabels,
-				},
-				Replicas: pulumi.Int(1),
-				Template: &corev1.PodTemplateSpecArgs{
-					Metadata: &metav1.ObjectMetaArgs{
-						Labels: appLabels,
-					},
-					Spec: &corev1.PodSpecArgs{
-						Containers: corev1.ContainerArray{
-							corev1.ContainerArgs{
-								Name:  pulumi.String("nginx"),
-								Image: pulumi.String("nginx"),
-							}},
-					},
-				},
-			},
-		})
-		if err != nil {
-			return err
+		if envFactory.Environment.Kind == "prod" {
+			//Create a GKE Cluster in Google Cloud
+			// Obtain credentials
+			// use gcloud and credentials to connect to the newly created cluster
+			// install Helm Chart for conference app
 		}
 
-		ctx.Export("name", deployment.Metadata.Elem().Name())
+		if envFactory.Environment.Kind == "dev" {
+			namespace, err := corev1.NewNamespace(ctx, envFactory.Environment.Name, nil)
 
+			vClusterRelease, err := helm.NewRelease(ctx, envFactory.Environment.Name, &helm.ReleaseArgs{
+				Chart:     pulumi.String("vcluster"),
+				Namespace: namespace.Metadata.Name(),
+				RepositoryOpts: &helm.RepositoryOptsArgs{
+					Repo: pulumi.String("https://charts.loft.sh"),
+				},
+				Version: pulumi.String("0.10.2"),
+				Values: pulumi.Map{
+					"Syncer": pulumi.Map{
+						"extraArgs": pulumi.Map{
+							"--out-kube-config-secret": pulumi.String(envFactory.Environment.Name + "-secret"),
+						},
+					},
+				},
+			})
+
+			vClusterSecretData := pulumi.All(vClusterRelease.Status.Namespace(), vClusterRelease.Status.Name()).ApplyT(func(r interface{}) interface{} {
+				arr := r.([]interface{})
+				namespace := arr[0].(*string)
+				name := arr[1].(*string)
+				vClusterSecret, err := corev1.GetSecret(ctx, "secret", pulumi.ID(fmt.Sprintf("%s/vc-%s", *namespace, *name)), nil)
+				if err != nil {
+					return ""
+				}
+				t := vClusterSecret.Data.ApplyT(func(r interface{}) string {
+					secretMap := r.(map[string]string)
+					payload, err := decodePayload([]byte(secretMap["config"]))
+					if err != nil {
+						return ""
+					}
+					s := string(payload)
+
+					log.Println("Config \n" + s)
+					return s
+				})
+
+				return t
+			})
+
+			ctx.Export("vClusterSecretData", vClusterSecretData)
+
+			//client-go port- forward https://stackoverflow.com/questions/59027739/upgrading-connection-error-in-port-forwarding-via-client-go
+
+			portForward(ctx, vClusterRelease, vClusterSecretData)
+
+			if err != nil {
+				return err
+			}
+
+			return nil
+		}
 		return nil
 	})
+	return nil
+}
+
+func portForward(ctx *pulumi.Context, vClusterRelease *helm.Release, vClusterSecretData pulumi.Output) {
+
+	stopCh := make(<-chan struct{})
+	readyCh := make(chan struct{})
+
+	kubeconfig := filepath.Join(
+		os.Getenv("HOME"), ".kube", "config",
+	)
+	config, err := clientCmd.BuildConfigFromFlags("", kubeconfig)
+	if err != nil {
+		log.Fatal(err)
+	}
+	flag.Parse()
+
+	// create the clientset
+	clientset, err := clientKubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pulumi.All(vClusterRelease.Status.Namespace(), vClusterRelease.Status.Name()).ApplyT(func(r interface{}) (interface{}, error) {
+		arr := r.([]interface{})
+		namespace := arr[0].(*string)
+		name := arr[1].(*string)
+		log.Println("Namespace/Service: " + fmt.Sprintf("%s/%s", *namespace, *name))
+
+		reqURL := clientset.CoreV1().RESTClient().Post().
+			Resource("pods").
+			Namespace(fmt.Sprintf("%s", *namespace)).
+			Name(fmt.Sprintf("%s-0", *name)).
+			SubResource("portforward").URL()
+
+		log.Println("URL : " + fmt.Sprintf("%s", reqURL))
+
+		transport, upgrader, err := spdy.RoundTripperFor(config) // I need to get the Kubeconfig for the Host Cluster
+		if err != nil {
+			log.Println("ERROR in RoundTripperFor")
+			log.Fatal(err)
+		}
+		dialer := spdy.NewDialer(upgrader, &http.Client{Transport: transport}, http.MethodPost, reqURL)
+		fw, err := portforward.New(dialer, []string{"8443:8443"}, stopCh, readyCh, os.Stdout, os.Stdout)
+		if err != nil {
+			log.Println("ERROR in NewDialer")
+			log.Fatal(err)
+		}
+		if err := fw.ForwardPorts(); err != nil {
+			log.Println("ERROR in ForwardPorts")
+			log.Fatal(err)
+		}
+
+		return nil, nil
+	})
+
+	go func() {
+		<-readyCh
+
+		args := kubernetes.ProviderArgs{
+			Kubeconfig: pulumi.Sprintf("%s", vClusterSecretData),
+		}
+		provider, err := kubernetes.NewProvider(ctx, "vcluster", &args) //use  vClusterSecretData here
+		if err != nil {
+			log.Println("ERROR in NewProvider")
+			log.Fatal(err)
+		}
+
+		installConferenceApplication(ctx, provider)
+
+
+
+		<-stopCh
+	}()
+}
+func installConferenceApplication(ctx *pulumi.Context, provider *kubernetes.Provider){
+	helm.NewRelease(ctx, "conference", &helm.ReleaseArgs{
+		Chart: pulumi.String("fmtok8s-conference-chart"),
+		RepositoryOpts: &helm.RepositoryOptsArgs{
+			Repo: pulumi.String("https://salaboy.github.io/helm/"),
+		},
+		Version:   pulumi.String("v0.1.1"),
+		Values:    pulumi.Map{},
+		SkipAwait: pulumi.BoolPtr(true),
+	}, pulumi.Provider(provider))
+}
+
+func decodePayload(body []byte) ([]byte, error) {
+	//Base64 Decode
+	b64 := make([]byte, base64.StdEncoding.DecodedLen(len(body)))
+	n, err := base64.StdEncoding.Decode(b64, body)
+	if err != nil {
+		return nil, err
+	}
+	return b64[:n], nil
 }
